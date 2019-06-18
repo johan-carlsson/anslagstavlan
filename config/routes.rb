a53a0e287092771ddcf9b337965b082037fa0573
Johan Carlsson <johan.carlsson@redpill-linpro.com>
2019-06-18 06:42:53 +0200

    Generated device controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :messages
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
   # Todo add    root to: "home#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
