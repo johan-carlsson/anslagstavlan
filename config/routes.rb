b98b1ceba333c5d47e6c6d398ab82b0b7057902c
Johan Carlsson <johan.carlsson@redpill-linpro.com>
2019-06-18 07:01:31 +0200

    Set root route to /messages

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
 
-  # Todo add    root to: "home#index"
+  root to: "messages#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
