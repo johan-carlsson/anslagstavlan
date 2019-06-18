96408f09c4e67ff4da47d172496fbb40a7b60a3b
Johan Carlsson <johan.carlsson@redpill-linpro.com>
2019-06-18 06:26:24 +0200

    Generated scaffold for Message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_060641) do
+ActiveRecord::Schema.define(version: 2019_06_18_042529) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string "subject"
+    t.string "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
